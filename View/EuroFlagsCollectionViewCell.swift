1d093e8e602baaafe2da907479a3b9136dec818e
Mohamad-hazz <mohammad.hazimeh@hotmail.com>
2024-10-26 23:24:20 +0200

    Initial commit

---
+++ View/EuroFlagsCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  EuroFlagsCollectionViewCell.swift
+//  EuroFlags
+//
+//  Created by Mohammad Hazimeh on 26/10/2024.
+//
+
+import UIKit
+
+class EuroFlagsCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var countryImageView: UIImageView!
+    
+    @IBOutlet weak var countryNameLabel: UILabel!
+    
+    @IBOutlet weak var countryCapitalLabel: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+   
+  
+        
+    }
+
